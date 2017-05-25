3395806c652373aade1a01da5432f57c12e7611b
MiollanAlbigot <ptjavamiollanalbigot@gmail.com>
2017-05-25 13:57:43 +0200

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -1,42 +1,42 @@
-public class Plateau {
+public class Echiquier {
 	
-	private Piece plateau[][] = new Piece[8][8];
+	private Piece echiquier[][] = new Piece[8][8];
 	
-	public Plateau(){
-		this.plateau = new Piece[8][8];
+	public Echiquier(){
+		this.echiquier = new Piece[8][8];
 	}
 
 	public void init (){
 		for(int x =1 ; x<2; x++){
 			for(int y = 0; y<8; y++){
-				plateau[x][y] = new Pion(0,false,x,y);
+				echiquier[x][y] = new Pion(0,false,x,y);
 			}
 		}
 		for(int x =6 ; x<7; x++){
 			for(int y = 0; y<8; y++){
-				plateau[x][y] = new Pion(1,false,x,y);
+				echiquier[x][y] = new Pion(1,false,x,y);
 			}
 		}
-		plateau[0][0] = new Tour(0, false,0,0);
-		plateau[0][7] = new Tour(0,false,0,7);
-		plateau[7][0] = new Tour(1,false,7,0);
-		plateau[7][7] = new Tour(1,false,7,7);
+		echiquier[0][0] = new Tour(0, false,0,0);
+		echiquier[0][7] = new Tour(0,false,0,7);
+		echiquier[7][0] = new Tour(1,false,7,0);
+		echiquier[7][7] = new Tour(1,false,7,7);
 		
-		plateau[0][1] = new Cavalier(0,false,0,1);
-		plateau[0][6] = new Cavalier(0,false,0,6);
-		plateau[7][1] = new Cavalier(1,false,7,1);
-		plateau[7][6] = new Cavalier(1,false,7,6);
+		echiquier[0][1] = new Cavalier(0,false,0,1);
+		echiquier[0][6] = new Cavalier(0,false,0,6);
+		echiquier[7][1] = new Cavalier(1,false,7,1);
+		echiquier[7][6] = new Cavalier(1,false,7,6);
 		
-		plateau[0][2] = new Fou(0,false,0,2);
-		plateau[0][5] = new Fou(0,false,0,5);
-		plateau[7][2] = new Fou(1,false,7,2);
-		plateau[7][5] = new Fou(1,false,7,5);
+		echiquier[0][2] = new Fou(0,false,0,2);
+		echiquier[0][5] = new Fou(0,false,0,5);
+		echiquier[7][2] = new Fou(1,false,7,2);
+		echiquier[7][5] = new Fou(1,false,7,5);
 		
-		plateau[0][4]= new Dame(0,false,0,4);
-		plateau[7][4]= new Dame(1,false,7,4);
+		echiquier[0][4]= new Dame(0,false,0,4);
+		echiquier[7][4]= new Dame(1,false,7,4);
 		
-		plateau[0][3] = new Roi(0,false,0,3);
-		plateau[7][3] = new Roi(1,false,7,3);
+		echiquier[0][3] = new Roi(0,false,0,3);
+		echiquier[7][3] = new Roi(1,false,7,3);
 
 		
 	}
@@ -45,7 +45,7 @@ public class Plateau {
 		for(int x = 0 ; x < 8 ; x++){
 			System.out.println("");
 			for(int y = 0 ; y < 8 ; y++){
-				System.out.print( "|| "+plateau[x][y]+" ||");
+				System.out.print( "|| "+echiquier[x][y]+" ||");
 			}
 		}
 	}
